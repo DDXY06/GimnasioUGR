6f3f5c768e5968938aabaedb496f1af356fb85cc
DDXY06 <davidxieye@gmail.com>
2026-05-05 23:05:58 +0200

    first commit

---
+++ src/main/java/com/example/gimnasiougr/GimnasiougrApplication.java
@@ -0,0 +1,13 @@
+package com.example.gimnasiougr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimnasiougrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimnasiougrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gimnasiougr/GimnasiougrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gimnasiougr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasiougrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
